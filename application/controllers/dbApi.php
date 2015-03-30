64f4e6c3ee6884f64c555d13ed3b14f022c7d156
Dinesh Devkota <dinesh.devkota@selu.edu>
2015-03-29 20:24:50 -0500

    Comment for files.

--- application/controllers/dbApi.php
+++ application/controllers/dbApi.php
@@ -3,11 +3,12 @@
 class dbApi extends CI_Controller {
 
 	/*
+	 * Controller for backend API. To be consumed by AngularJS
 	 */
 	public function index()
 	{
 		$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
-return json_encode($arr);
+           echo json_encode($arr);
 	}
 	
 	
