6963c9ec25bdc1f5d84881e71dd0f6ed528ea067
Dinesh Devkota <dinesh.devkota@selu.edu>
2015-05-10 22:26:44 -0500

    documentation as welcome page.

--- application/views/navviews/welcome.php
+++ application/views/navviews/welcome.php
@@ -2,7 +2,41 @@
 <h1>Welcome to CMPS 439 The Database Class Project</h1>
 
 <div id="body">
-	<p>We gonna do some DB demonstration here. Lots of datatable :D</p>
-	<div id="cy"></div>
+	<p>This views are based on following JSON API endpoints.</p>
+	<div class="link-lists">
+	<a href="<?php echo base_url(); ?>dbApi/GetWarpedTours">GetWarpedTours</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetStages">GetStages</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetAlbums">GetAlbums</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetArtists">GetArtists</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetBands">GetBands</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetGenres">GetGenres</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetOrganizers">GetOrganizers</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetRodies">GetRodies</a>
+	<a href="<?php echo base_url(); ?>dbApi/>GetSongs">GetSongs</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetSponsors">GetSponsors</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetStageByWarpedTourID/2">GetStageByWarpedTourID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetWarpedTourByStageID/3">GetWarpedTourByStageID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetOrganizerByWarpedTourID/4">GetOrganizerByWarpedTourID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetWarpedTourByOrganizerID/2">GetWarpedTourByOrganizerID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetSponserByWarpedTourID/1">GetSponserByWarpedTourID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetWarpedTourBySponsorID/2">GetWarpedTourBySponsorID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetBandByWarpedTourID/2">GetBandByWarpedTourID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetWarpedTourByBandID/2">GetWarpedTourByBandID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetSponsorByStageID/2">GetSponsorByStageID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetStageBySponsorID/3">GetStageBySponsorID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetAlbumsByBandID/3">GetAlbumsByBandID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetSongsByAlbumID/1">GetSongsByAlbumID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetSongsByBandID/3">GetSongsByBandID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetBandsByStageID/1">GetBandsByStageID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetSongsWithDetailsByAlbumID/2">GetSongsWithDetailsByAlbumID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetArtistsWithDetails">GetArtistsWithDetails</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetSongsWithDetails">GetSongsWithDetails</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetSongsWithDetailsRandom">GetSongsWithDetailsRandom</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetBandAndGenreByWarpedTourID/1">GetBandAndGenreByWarpedTourID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetArtistByBandID/1">GetArtistByBandID/#</a>
+	<a href="<?php echo base_url(); ?>dbApi/GetRodiesByBandID/1">GetRodiesByBandID/#</a>
+
+	
+	</div>
 </div>
 
